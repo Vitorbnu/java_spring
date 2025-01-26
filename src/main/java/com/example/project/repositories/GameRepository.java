8efeaba9cb8716c9b13d7d772f97e25046f8d4f6
Vitor da Silva <72954077+Vitorbnu@users.noreply.github.com>
2025-01-26 16:09:13 -0300

    GameMinDTO, GameRepository, GameService and GameController

---
+++ src/main/java/com/example/project/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package com.example.project.repositories;
+
+import com.example.project.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
---
+++ src/main/java/com/example/project/services/GameService.java
@@ -0,0 +1,22 @@
+package com.example.project.services;
+
+import com.example.project.dto.GameMinDTO;
+import com.example.project.entities.Game;
+import com.example.project.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    public GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+}
